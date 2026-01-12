f668bf17d2b551b40acb785238b9d1811fef5577
Kxi723 <guanxi723@gmail.com>
2026-01-12 09:32:15 +0800

    12/1 9pm32

--- app.component.ts
+++ app.component.ts
@@ -1,5 +1,117 @@
-import { Component, signal, computed } from '@angular/core';
-import { FormsModule } from '@angular/forms'
+import { CommonModule, NgFor } from '@angular/common';
+import { Component, OnInit, signal, computed, ViewChild, AfterViewInit, ElementRef } from '@angular/core';
+import { FormGroup, FormBuilder, FormArray, FormControlName, Validators, FormsModule, NgForm, RequiredValidator, FormControl, NgModel, ReactiveFormsModule } from '@angular/forms'
+import { JsonPipe } from '@angular/common';
+
+@Component ({
+  selector:'app-root',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl:'./app.component.html',
+  styles:['div {background-color: #f2f2f2; padding: 15px; margin: 5px}','p{margin: 0px;}']
+})
+export class AppComponent implements OnInit {
+  public _parentForm!: FormGroup;
+  public _name!:FormGroup;
+  public _addr!: FormGroup;
+  public _items!: FormArray;
+  constructor(private _fb: FormBuilder) {}
+  ngOnInit(){
+    this._name = this._fb.group({
+      fname:['',[Validators.required]],
+      lname:['',[Validators.required]]
+    });
+    
+    this._addr = this._fb.group({
+      addr1:['',[Validators.required]],
+      addr2:[''],
+      city:['',[Validators.required]],
+      state:['',[Validators.required]],
+      zip:['',[Validators.required, Validators.pattern(/^\d{5}$/)]]
+    });
+
+    this._items = this._fb.array ([this.createItemFormGroup()]);
+
+    this._parentForm = this._fb.group({
+      name: this._name,
+      addr: this._addr,
+      items: this._items
+    });
+  }
+
+  createItemFormGroup() {
+    return this._fb.group({
+      name:['', Validators.required],
+      qty:['1', Validators.required],
+      price:['', Validators.required]
+    }); 
+  }
+
+  addItem(){this._items.push(this.createItemFormGroup());}
+
+  deleteItem(index: number) { this._items.removeAt(index); }
+
+  onSubmit(form:FormGroup){alert('Submiited');}
+}
+
+// @Component ({
+//   selector:'app-root',
+//   standalone: true,
+//   imports: [ReactiveFormsModule],
+//   template: `
+//   <form #form [formGroup]="formGroup" (ngSubmit)="onSubmit(formGroup)" novalidate>
+//     <label>Name:<input formControlName="name"> </label>
+//     <br/>
+//     <label>Location:<input formControlName="location"> </label>
+//     <br/>
+//     <input type="submit" value="Submit" [disabled]="!formGroup.valid">
+//   </form>`,
+//   styles: []
+// })
+// export class AppComponent implements OnInit {
+//   formGroup!: FormGroup;
+//   ngOnInit(){
+//     this.formGroup = new FormGroup({
+//       name: new FormControl('', Validators.required),
+//       location: new FormControl('', Validators.required)
+//     });
+//   }
+//   onSubmit(form:FormGroup) {
+//     alert('submit');
+//   }
+// }
+
+// @Component ({
+//   selector: 'app-root',
+//   standalone: true,
+//   imports: [FormsModule, CommonModule],
+//   template: `
+//   <form #f="ngForm" novalidate>
+//     <p>
+//       <label>First Name</label>
+//       <input name="fname" ngModel #fname="ngModel" required />
+//       <span class="error" *ngIf="fname.touched && fname.hasError('required')">Required</span>
+//     </p>
+//     <p>
+//       <label>Last Name</label>
+//       <input name="lname" ngModel #lname="ngModel" required />
+//       <span class="error" *ngIf="lname.touched && lname.hasError('required')">Required</span>
+//     </p>
+//     <p>
+//       <label>Email</label><input name="email" ngModel #email="ngModel" required email/>
+//       <span class="error" *ngIf="email.value && email.touched && email.hasError('email')">Invalid email </span>
+//     </p>
+//     <button (click)="onSubmit()" [disabled]="!f.valid"> Submit</button>
+//   </form>`,
+// styles: []
+// })
+// export class AppComponent{
+//   onSubmit() {
+//     alert('Submitted');
+//   }
+// }
+
+// ================================================================================================================================================================
 
 // @Component({
 //   selector: 'app-root',
@@ -94,23 +206,167 @@ import { FormsModule } from '@angular/forms'
 //   }
 // }
 
-@Component ({
-  selector:'app-root', 
-  standalone: true,
-  template:`
-  <h1> {{title}} </h1>
-  <p>Length: {{title.length}}</p>
-  <p>Resersed: {{getReversed(title)}}</p>`,
-  styles: []
-})
-export class AppComponent{title = 'welcome to app!'
+// @Component ({
+//   selector:'app-root', 
+//   standalone: true,
+//   template:`
+//   <h1> {{title}} </h1>
+//   <p>Length: {{title.length}}</p>
+//   <p>Resersed: {{getReversed(title)}}</p>`,
+//   styles: []
+// })
+// export class AppComponent{
+//     title = 'welcome to app!'
 
-getReversed (str:string){
-  let reversed ='';
-  for (let i = str.length-1;i>=0;i--){
-    reversed +=str.substring(i,i+1);
+//     getReversed (str:string){
+//         let reversed ='';
+//         for (let i = str.length-1;i>=0;i--){
+//             reversed +=str.substring(i,i+1);
+//         }
+//     return reversed
+//     }
+// }
 
-  }
-  return reversed
-}
-}
+// @Component ({
+//     selector:'app-root',
+//     standalone: true,
+//     template: `
+//     <h1>Doesnt work: </h1>
+//     <img src="startUrl">
+//     <h1>Works:</h1>
+//     <img [src]="startUrl">`,
+//     styles: []
+// })
+// export class AppComponent {
+//     startUrl = 'https://developer.mozilla.org/samples/cssref/images/starsolid.gif';
+// }
+
+// @Component ({
+//     selector: 'app-root',
+//     standalone: true,
+//     template: `
+//     <input #input type = "text" (input)="textInput($event)" value=""/>
+//     <hr>
+//     Upper-Case: {{upperCase}} <br/>
+//     Lower-Case: {{lowerCase}} `,
+//     styles: []
+// })
+// export class AppComponent implements AfterViewInit {
+//     upperCase: string = '';
+//     lowerCase: string = '';
+//     @ViewChild('input') inputBox!: ElementRef<HTMLInputElement>; // we didnt declare 'inputBox' a type, so TypeScript not allow this, so need declare ElementRef
+//     textInput(event: Event) { // this also declare an Event type to 'event'
+//         const value = (event.target as HTMLInputElement).value;
+//         this.upperCase = value.toUpperCase();
+//         this.lowerCase = value.toLowerCase();
+//     }
+//     ngAfterViewInit() {
+//         this.inputBox.nativeElement.focus()
+//     }
+// }
+
+// @Component ({
+//     selector:'app-root',
+//     standalone: true,
+//     imports: [FormsModule, JsonPipe],
+//     template: `
+//     <form #f="ngForm" novalidate>
+//     <p>First Name<input name="fname" ngModel required/></p>
+//     <p>Second Name<input name="lname" ngModel required/></p>
+//     Valid:{{f.valid}}    Data:{{f.value|json}} </form>
+//     `,
+//     styles: []
+// })
+// export class AppComponent { 
+//     @ViewChild('f') f!:NgForm;
+// }
+
+// @Component ({
+//     selector:'app-root',
+//     standalone: true,
+//     imports: [FormsModule, JsonPipe],
+//     template: `
+//     <form #f="ngForm" novalidate>
+//     <p>First Name<input name="fname" ngModel #fname="ngModel" required/></p>
+//     <h2>Form Template Variable</h2>
+//         Valid:{{fname.valid}}
+//         Data:{{fname.value|json}}
+//         <h2>From Instance Variable</h2>
+//         Valid:{{fname2?.valid}}
+//         Data:{{fname2?.value|json}} 
+//     </form>`,
+//     styles: []
+// })
+// export class AppComponent { 
+//     @ViewChild('f') f!:NgForm;
+//     @ViewChild('fname') fname2!:NgModel;
+// }   
+
+// @Component({
+//   selector: 'app-root',
+//   standalone: true,
+//   imports: [FormsModule],
+//   template: `
+//   <input type="text" class="form-control" name="name" placeholder="Name (last,first)" [(ngModel)]="_name" required>`,
+//   styles: []
+// })
+// export class AppComponent {
+//   _name: string = '2';
+// } 
+
+// @Component({
+//   selector: 'app-root',
+//   standalone: true,
+//   imports: [FormsModule, JsonPipe],
+//   template: `
+//   <form #appointmentForm="ngForm" novalidate (ngSubmit)="onSubmitForm(appointmentForm)"> 
+//     <legend>Appointment</legend>
+//     <div class="form-group">
+//       <label for="name">Name</label>
+//       <input type="text" class="form-control" name="name" placeholder="Name(last,first)" [(ngModel)]="_name" required>
+//     </div>
+//     <div class="form-group">
+//       <label for="password">Password</label>
+//       <input type="password" class="form-control" name="password" placeholder="Password" [(ngModel)]="_password" required>
+//     </div>
+//     <div class="form-group">
+//       <div class="form-check">
+//         <div><label>Appointment</label></div>
+//           <label class="form-check-label">
+//             <input type="radio" class="form-check-input" name="time" value="12pm" [(ngModel)]="_time" required>12pm
+//           </label>
+//       </div>
+//       <div class="form-check">
+//         <label class="form-check-label">
+//           <input type="radio" class="form-check-input" name="time" value="2pm" [(ngModel)]="_time" required>2pm
+//         </label>
+//       </div>
+//       <div class="form-check">
+//         <label class="form-check-label">
+//           <input type="radio" class="form-check-input" name="time" value="4pm" [(ngModel)]="_time" required>4pm
+//         </label>
+//       </div>
+//     </div>
+//     <div class="form-group">
+//       <label for="exampleTextarea">Ailment</label>
+//       <textarea class="form-control" name="ailment" rows="3" [(ngModel)]="_ailment" required></textarea>
+//     </div>
+//     <button type="submit" class="btn btn-primary" [disabled]="!_appointmentForm?.valid">Submit</button>
+//     <p>Valid:{{_appointmentForm?.valid}}</p>
+//     <p>Data:{{_appointmentForm?.value|json}}</p>
+//   </form>    
+//   `,
+//   styles: ['form{padding: 20px}','.form-group {padding-top: 20px}']
+// })
+// export class AppComponent {
+//   @ViewChild('appointmentForm') _appointmentForm!:NgForm;
+//   _name:string='';
+//   _password:string='';
+//   _time:string='';
+//   _ailment:string='';
+//   onSubmitForm(_appointmentForm:NgForm) {
+//     alert("Submitting data:"+ JSON.stringify(this._appointmentForm.value));
+//   }
+// } 
+
+// stop right here --------------------------------------------------------------------------------------------------------------------------------------
